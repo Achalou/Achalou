40dcb1aafff34027d412cd46b9ef25939d42c505
Achalou <achalou24@gmail.com>
2026-09-24 18:39:58 +0200

    Ajout d’un concert via A’Cha Lou Admin

--- concerts.js
+++ concerts.js
@@ -1,14 +1,12 @@
 /*
- A’CHA LOU — LES CONCERTS
-
- Pour ajouter une date, COPIE simplement un bloc entre { } et change les informations.
- La date doit être écrite AAAA-MM-JJ.
-
- Exemple :
- { date: "2026-10-12", lieu: "Gazette Café · Montpellier", heure: "20h30", info: "" },
-
- Tu peux mettre plusieurs concerts : le site choisit automatiquement le prochain.
+ A’CHA LOU — CONCERTS
+ Fichier géré automatiquement par l’administration.
 */
 window.ACHALOU_CONCERTS = [
-  // Ajoute les nouvelles dates ici.
+  {
+    "date": "2026-12-12",
+    "lieu": "olympia · serignan",
+    "heure": "20:00",
+    "info": "pouet"
+  }
 ];

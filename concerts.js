7c5b12b9e12f126f89f884cc04b44c4f2e90ad32
Achalou <achalou24@gmail.com>
2026-09-25 15:11:45 +0200

    Suppression d’un concert via A’Cha Lou Admin

--- concerts.js
+++ concerts.js
@@ -2,11 +2,4 @@
  A’CHA LOU — CONCERTS
  Fichier géré automatiquement par l’administration.
 */
-window.ACHALOU_CONCERTS = [
-  {
-    "date": "2026-12-12",
-    "lieu": "au bal de la maison d a cote aqui est en face de derriere · saint jean de la bredouille au site alambique",
-    "heure": "20:00",
-    "info": "ramene ta fraise et des petits choux a la creme au fond de dinette"
-  }
-];
+window.ACHALOU_CONCERTS = [];

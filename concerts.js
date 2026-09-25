abcec658926ef23ae2b1decac5b3446383ed2b52
Achalou <achalou24@gmail.com>
2026-09-25 18:31:07 +0200

    Suppression d’un concert via A’Cha Lou Admin

--- concerts.js
+++ concerts.js
@@ -2,11 +2,4 @@
  A’CHA LOU — CONCERTS
  Fichier géré automatiquement par l’administration.
 */
-window.ACHALOU_CONCERTS = [
-  {
-    "date": "2026-12-12",
-    "lieu": "dvklhdvhpduhpdpv dcafe · vjdlkjvdjhdlkhvdmlkvd montp",
-    "heure": "20:00",
-    "info": "dlgkhd^gihd^ghd"
-  }
-];
+window.ACHALOU_CONCERTS = [];

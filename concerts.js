ef830878f6caec8b7b923d901ec9824d5c574d69
Achalou <achalou24@gmail.com>
2026-09-25 15:57:08 +0200

    Ajout d’un concert via A’Cha Lou Admin

--- concerts.js
+++ concerts.js
@@ -2,4 +2,11 @@
  A’CHA LOU — CONCERTS
  Fichier géré automatiquement par l’administration.
 */
-window.ACHALOU_CONCERTS = [];
+window.ACHALOU_CONCERTS = [
+  {
+    "date": "2026-12-20",
+    "lieu": "ksjfgtrebslmlsmdmsfhlmfsk · fsmflkjsklshukgffbsmonpt",
+    "heure": "20:00",
+    "info": "npway"
+  }
+];

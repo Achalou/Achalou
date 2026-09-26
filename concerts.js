3b66c2c6fbaf5c3254b402a81882d0638b9f4ef9
Achalou <achalou24@gmail.com>
2026-09-26 03:51:47 +0200

    Suppression définitive d’un concert via A’Cha Lou Admin

--- concerts.js
+++ concerts.js
@@ -5,19 +5,6 @@
 window.ACHALOU_CONCERTS = [
   {
     "date": "2026-12-12",
-    "lieu": "gazette au bout de la rue · montpellier ville collabo",
-    "heure": "20:00",
-    "info": "pouet pouet pouet",
-    "statut": "publie",
-    "publicationProgrammee": "",
-    "etat": "annule",
-    "raisonEtat": "le ciel pleut comme vache qui pisse",
-    "heureAvant": "",
-    "lieuAvant": "",
-    "supprimeLe": "2026-09-26T01:51:39.227Z"
-  },
-  {
-    "date": "2026-12-12",
     "lieu": "Gazette · Montp",
     "heure": "20:00",
     "info": "test",

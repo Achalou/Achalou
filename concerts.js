0907036029d5c8d98bbb67644f7129c4c882f59e
Achalou <achalou24@gmail.com>
2026-09-26 16:06:23 +0200

    Suppression définitive d’un concert via A’Cha Lou Admin

--- concerts.js
+++ concerts.js
@@ -17,21 +17,5 @@ window.ACHALOU_CONCERTS = [
     "lieuAvant": "",
     "statut": "publie",
     "publicationProgrammee": ""
-  },
-  {
-    "date": "2026-12-13",
-    "lieu": "gaz · montp",
-    "heure": "21:00",
-    "info": "oui",
-    "etat": "reporte",
-    "raisonEtat": "il pleut comme vache qui pisse",
-    "heureAvant": "",
-    "lieuAvant": "",
-    "statut": "publie",
-    "publicationProgrammee": "",
-    "supprimeLe": "2026-09-26T14:05:04.911Z",
-    "nouvelleDate": "2026-12-13",
-    "nouvelleHeure": "21:00",
-    "nouveauLieu": "palavas"
   }
 ];

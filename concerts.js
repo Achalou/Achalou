86ec29e95fcbd3bfb239892ab5206399b199f703
Achalou <achalou24@gmail.com>
2026-09-26 18:06:45 +0200

    Suppression définitive d’un concert via A’Cha Lou Admin

--- concerts.js
+++ concerts.js
@@ -2,23 +2,4 @@
  A’CHA LOU — CONCERTS
  Fichier géré automatiquement par l’administration.
 */
-window.ACHALOU_CONCERTS = [
-  {
-    "date": "2026-12-12",
-    "lieu": "gazette cafe aux bois d artichaud · saint jean de cuculles",
-    "heure": "20:00",
-    "info": "va voir ta gueule a la recre",
-    "etat": "annule",
-    "raisonEtat": "yen a marre",
-    "nouvelleDate": "2026-12-13",
-    "nouvelleHeure": "21:00",
-    "nouveauLieu": "",
-    "heureAvant": "",
-    "lieuAvant": "",
-    "statut": "publie",
-    "publicationProgrammee": "",
-    "supprimeLe": "2026-09-26T16:06:31.933Z",
-    "nouvelleVille": "",
-    "complet": true
-  }
-];
+window.ACHALOU_CONCERTS = [];

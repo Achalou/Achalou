38dd16f3687c984096573c044551b8cecb441876
Achalou <achalou24@gmail.com>
2026-09-26 19:15:36 +0200

    Ajout d’un concert via A’Cha Lou Admin

--- concerts.js
+++ concerts.js
@@ -2,4 +2,22 @@
  A’CHA LOU — CONCERTS
  Fichier géré automatiquement par l’administration.
 */
-window.ACHALOU_CONCERTS = [];
+window.ACHALOU_CONCERTS = [
+  {
+    "date": "2026-12-12",
+    "lieu": "Gazette cafe au bal · Monpellier des liens",
+    "heure": "20:00",
+    "info": "",
+    "etat": "normal",
+    "complet": false,
+    "raisonEtat": "",
+    "nouvelleDate": "",
+    "nouvelleHeure": "",
+    "nouveauLieu": "",
+    "nouvelleVille": "",
+    "heureAvant": "",
+    "lieuAvant": "",
+    "statut": "publie",
+    "publicationProgrammee": ""
+  }
+];
